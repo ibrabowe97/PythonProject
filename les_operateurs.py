ff7ca299a45f8e5a8447a66feec33df422fdaacc
ibrahim <ibrabowe97@yahoo.fr>
2022-11-18 16:03:54 +0000

    les operateurs en python

---
+++ les_operateurs.py
@@ -0,0 +1,15 @@
+#coding:utf-8
+
+"""
+les operateurs en python sont:
+                        addition : +
+                        soustraction : -
+                        multiplication : *
+                        division : /
+                        reste de la division entiere (modulo): % 
+"""
+calcul = 5.0/2.0
+calcul = int (calcul)
+calcul2 = 5 % 2
+
+print("votre calcul vaut :", calcul, "reste vaut :", calcul2)
