6357695da26b00c5d068712723f0899ad8f04a5a
ibrahim <ibrabowe97@yahoo.fr>
2022-12-04 21:16:46 +0000

    les conditions en pythons

---
+++ les_conditions.py
@@ -0,0 +1,21 @@
+#encoding:utf-8
+
+id="ibrahima"
+mdp="bowePlus"
+
+print("interface connexion")
+user_id=input("entrer votre id:\n")
+user_mdp=input("saisir le mot de passe:\n")
+
+if id == user_id:
+    print("bienvenue Mr ", user_id)
+if mdp == user_mdp:
+    print("votre mot de passe est correct")
+age=input("donnez votre age:\n")
+age=int(age);
+if 0 < age< 18:
+    print("vous etes pre-adultes\n");
+elif 18<= age < 50:
+    print("vous etes adultes\n");
+else:
+    print("vous etes vieux\n");
