4d794a241df0997709ac9a8bd5e2c260455fa091
Ibrahima Diallo <ibrabowe97@gmail.com>
2023-02-16 12:51:38 +0000

    renommage des fichiers

---
+++ operateurs.py
@@ -0,0 +1,18 @@
+#coding:utf-8
+
+"""
+les operateurs en python sont:
+        addition                       : +
+        soustraction                   : -
+        multiplication                 : *
+        division                       : /
+        division entiere               : //
+        reste d'une division (modulo)  : %
+        difference                     : != 
+        le negatif                     : ! 
+"""
+calcul = 5.0/2.0
+calcul = int (calcul)
+calcul2 = 5 % 2
+
+print("votre calcul vaut :", calcul, "et le reste vaut :", calcul2)
